8ab45e0d8ba045cbffc330f510a9a79123c6e2ab
Rehan Ahmad <rehanahmad150608@gmail.com>
2023-03-31 10:47:42 +0500

    added backend for rlabs

---
+++ pages/api/email.ts
@@ -0,0 +1,43 @@
+import type { NextApiRequest, NextApiResponse } from 'next';
+import NextCors from 'nextjs-cors';
+import nodemailer from 'nodemailer';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  await NextCors(req, res, {
+    methods: ['POST'],
+    origin: [
+      'http://localhost:3000',
+      'https://www.rlabs.xr',
+      'https://rlabs.xr',
+      'https://rlabs-web.vercel.app',
+    ],
+    optionsSuccessStatus: 200,
+  });
+  if (req.method === 'POST') {
+    if (!req.body.email || !req.body.subject || !req.body.text || !req.body.name)
+      return res.status(400).send('Missing fields');
+    try {
+      const transporter = nodemailer.createTransport({
+        host: 'smtp-relay.sendinblue.com',
+        port: 587,
+        secure: false,
+        auth: {
+          user: process.env.EMAIL_USER,
+          pass: process.env.EMAIL_PASS,
+        },
+      });
+      await transporter.sendMail({
+        from: {
+          name: req.body.name,
+          address: req.body.email,
+        },
+        to: process.env.EMAIL_USER,
+        subject: req.body.subject,
+        text: req.body.text,
+      });
+      res.send('Email sent');
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  }
+}
